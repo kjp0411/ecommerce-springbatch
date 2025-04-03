542d7bc1c16b6f3f82ca7fe4adb0a72e81c90f48
kjp0411 <gimjongpyo0411@hanmail.net>
2025-04-03 15:47:25 +0900

    상품 CSV 파일 생성 유틸리티 개발

---
+++ ecommerce-batch/src/test/java/fastcampus/ecommerce/batch/util/ReflectionUtilsTest.java
@@ -0,0 +1,25 @@
+package fastcampus.ecommerce.batch.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ReflectionUtilsTest {
+
+  private static class TestClass {
+
+    private String stringField;
+    private int intField;
+    public static final String CONSTANT = "constant";
+  }
+
+  @Test
+  void testGetFieldNames() {
+    List<String> fieldNames = ReflectionUtils.getFieldNames(TestClass.class);
+
+    assertThat(fieldNames).hasSize(2)
+        .contains("stringField", "intField")
+        .doesNotContain("CONSTANT");
+  }
+}
